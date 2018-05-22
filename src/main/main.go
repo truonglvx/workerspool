23cc5fa60e6b3f313ec0489c7bfd599243233529
truongle <truonglvx@gmail.com>
2018-05-22 18:05:45 +0700

    - add OS signal and exit

--- src/main/main.go
+++ src/main/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	
 	"fmt"
+	"os"
+	"os/signal"
 	"sync"
+	"syscall"
 	"time"
 	//"sort"
 )
@@ -28,20 +30,34 @@ func worker(workerID int, tasks chan Work) {
 	}
 }
 
+const tasksNum = 20
+const workersNum = 3
+
+func cleanUp(tasks chan Work) {
+
+	fmt.Println("clean up!!!")
+	close(tasks)
+
+}
+
 func main() {
 
 	wg := new(sync.WaitGroup)
+	s := make(chan os.Signal)
+	defer close(s)
+
+	signal.Notify(s, os.Interrupt, syscall.SIGHUP)
 
 	tasks := make(chan Work)
-	const tasksNum = 10
 
 	wg.Add(tasksNum)
 
 	go func() {
 		for i := 0; i < tasksNum; i++ {
 			tasks <- func(wokerId int) {
+				fmt.Println("doing task on worker", wokerId)
 				time.Sleep(500 * time.Millisecond)
-				fmt.Println("finished job on worker", wokerId)
+				fmt.Println("finished task on worker", wokerId)
 				wg.Done()
 			}
 		}
@@ -49,14 +65,20 @@ func main() {
 	}()
 
 	go func() {
-		for j := 0; j < 3; j++ {
+		for j := 0; j < workersNum; j++ {
 			go worker(j, tasks)
 		}
 	}()
 
+	go func() {
+		<-s
+		cleanUp(tasks)
+		os.Exit(1)
+
+	}()
+
 	wg.Wait()
 
-	
 	fmt.Println("done!!!")
 
 }
